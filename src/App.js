9d7ac005c2156efe91dde3d783cb7b23d94dc036
Fatih Yıldırım <isafatihyildirim@gmail.com>
2018-12-26 22:42:35 +0300

    edited app container

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import { Grid, Row } from 'react-flexbox-grid';
 import SearchBox from './components/SearchBox';
 import './App.css';
 
+import array from './components/data';
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { row: 5, column: 4 };
+  }
+
+  boxes = () => {
+    let i = null;
+    const { row, column } = this.state;
+    const boxes = [];
+
+    for (i = 0; i < (row * column); i += 1) {
+      boxes.push(<SearchBox key={i} data={array} />);
+    }
+
+    return boxes;
+  }
+
   render() {
+    const rowStyle = {
+      height: '100vh',
+    };
+
     return (
       <Grid fluid>
-        <Row><SearchBox /></Row>
+        <Row style={rowStyle}>
+          {this.boxes()}
+        </Row>
       </Grid>
     );
   }
